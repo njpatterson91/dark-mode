6de116e7d4dc85cafe5f2916b0ad1db56646c284
Nathaniel Patterson <odst0016@gmail.com>
2020-09-30 18:55:05 -0400

    first push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
@@ -16,8 +17,8 @@ const App = () => {
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
